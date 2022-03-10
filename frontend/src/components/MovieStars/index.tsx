4482fee843b14587880f51e014d570a62247acc9
Washington Junior <washjunior4444@gmail.com>
2022-03-09 23:00:26 -0400

    Update

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -1,6 +1,6 @@
-import {ReactComponent as StarFull} from 'Assets/img/star-full.svg';
-import {ReactComponent as StarHalf} from 'Assets/img/star-half.svg';
-import {ReactComponent as StarEmpty} from 'Assets/img/star-empty.svg';
+import {ReactComponent as StarFull} from 'assets/img/star-full.svg';
+import {ReactComponent as StarHalf} from 'assets/img/star-half.svg';
+import {ReactComponent as StarEmpty} from 'assets/img/star-empty.svg';
 import './styles.css';
 
 function MovieStars() {
